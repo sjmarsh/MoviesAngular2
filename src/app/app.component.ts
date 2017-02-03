6ea085fb1b847b9fe52557dd6a87d513a0929910
sjmarsh <sjmarsh31@gmail.com>
2017-02-03 16:50:10 +1100

    Update Styling for "Version 2" :two:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ import { MovieService } from './movie.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css']
 })
 
 export class AppComponent implements OnInit {
